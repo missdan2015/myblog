47ab466dfed87ee7194bf5eb2ea4d93821f9aa4b
missdan2015 <1070549419@qq.com>
2020-04-12 22:15:41 +0800

    主页内容

--- src/config/page.config.js
+++ src/config/page.config.js
@@ -1,40 +1,155 @@
 const mainList = [
     {
-        logo: '',
+        logo: 'static/images/logo1.png',
         name: 'Personal Information'
     },
     {
-        logo: '',
+        logo: 'static/images/logo3.png',
         name: 'JavaScript'
     },
     {
-        logo: '',
+        logo: 'static/images/logo3.png',
         name: 'CSS'
     },
     {
-        logo: '',
+        logo: 'static/images/logo3.png',
         name: 'React'
     },
     {
-        logo: '',
+        logo: 'static/images/logo3.png',
         name: 'LeetCode'
     },
     {
-        logo: '',
+        logo: 'static/images/logo2.png',
         name: 'Favorite Movies'
     },
     {
-        logo: '',
+        logo: 'static/images/logo2.png',
         name: 'Classic Books'
     },
     {
-        logo: '',
+        logo: 'static/images/logo4.png',
         name: 'Ablum'
     },
     {
-        logo: '',
+        logo: 'static/images/logo5.png',
         name: 'expand'
     },
 
 ];
-export { mainList };
+const textList = [
+    {
+        name: '',
+        detail: '主动才有故事，不主动故事就是别人的，当然也可能是事故。'
+    },
+    {
+        name: '',
+        detail: '我有时候真怀疑你脑子里住了一个科学家，你知道谁吗？瓦特。'
+    },
+    {
+        name: '',
+        detail: '说好一起出石头!你为什么出了布?所以当有些人傻乎乎的想输时，就已经赢了'
+    },
+    {
+        name: '',
+        detail: '日久不一定生情，但一定要见人心!'
+    }, 
+    {
+        name: '',
+        detail: '没有遇见喜欢的人，就先做喜欢的事。'
+    }, 
+    {
+        name: '',
+        detail: '不在意的样子是我最后的表演。'
+    },
+    {
+        name: '',
+        detail: 'I love you three thousand times.---by《复仇者联盟4》'
+    },
+];
+const friendsUrl = {
+    title: '友情链接',
+    list:    [
+            
+            {
+                name: '知乎',
+                value: 'https://zhuanlan.zhihu.com/'
+            },
+            {
+                name: '掘金',
+                value: 'https://juejin.im/'
+            },
+            {
+                name: '少数派',
+                value: 'https://sspai.com/'
+            },
+            {
+                name: '简书',
+                value: 'https://www.jianshu.com/'
+            },
+            {
+                name: '阿里图标库',
+                value: 'https://www.iconfont.cn/'
+            },
+        ]
+};
+
+const workOnline = {
+    title: '在线工具',
+    list:    [
+            {
+                name: '腾讯文档',
+                value: 'https://docs.qq.com/desktop'
+            }, 
+            {
+                name: '在线作图',
+                value: 'https://www.processon.com/'
+            },
+            {
+                name: 'Bable',
+                value: 'https://www.babeljs.cn/'
+            },
+            {
+                name: '语雀',
+                value: 'https://www.yuque.com/'
+            },
+            {
+                name: 'Json.cn',
+                value: 'https://www.json.cn/'
+            },
+        ]
+};
+
+const documents = { 
+    title: '在线文档',
+    list:    [
+            {
+                name: 'React小书',
+                value: 'http://huziketang.mangojuice.top/books/react/'
+            }, 
+            {
+                name: 'Ant Design',
+                value: 'https://ant.design/index-cn'
+            }, 
+            {
+                name: 'cnode',
+                value: 'https://cnodejs.org/'
+            },
+            {
+                name: '配色表',
+                value: 'http://tool.c7sky.com/webcolor/'
+
+            },
+            {
+                name: '微信官方文档',
+                value: 'https://developers.weixin.qq.com/doc/'
+            }
+        ]
+};
+export {
+    mainList,
+    textList,
+    friendsUrl,
+    workOnline,
+    documents
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,78 @@
 import React, { Component } from 'react';
 import './index.less';
-import { mainList } from '../config/page.config';
+import {
+    mainList,
+    textList,
+    friendsUrl,
+    workOnline,
+    documents
+} from '../config/page.config';
 import _ from 'lodash';
 
 export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showBackTop: false
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', () => {
+            let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            console.log('scrollTop', scrollTop)
+            if(scrollTop > 100) {
+                this.setState({
+                    showBackTop: true
+                })
+            } else {
+                this.setState({
+                    showBackTop: false
+                })
+            }
+        })
+    }
+    
+    goTop = () => {
+        let scrollToTop = window.setInterval(function () {
+            let pos = window.pageYOffset;
+            if (pos > 0) {
+                window.scrollTo(0, pos - 20); // how far to scroll on each step
+            } else {
+                window.clearInterval(scrollToTop);
+            }
+        }, 2);
+    }
+
     render() {
         return (
             <div className='wrap'> 
-               <div className='head'>头部</div>
+               <div className='head'>
+                    <img alt='' src = {require('../static/images/smile.png')} className = 'left' />
+                    < div className = 'right' >
+                        <ul>
+                            {
+                                _.map(textList, (text, index) => {
+                                    return(
+                                        <li key={index}>
+                                            <span>{text.detail}</span>
+                                        </li>
+                                    )
+                                }) 
+                            }
+                        </ul>
+                    </div>
+               </div>
+
                <div className='middle'>
                 {
                     _.map(mainList, (item, index) => {
+                        
                         return(
                             <div key={index} className='box'>
                                 <div className='up'></div>
                                 <div className='down'>
-                                    <span>{item.logo}</span>
+                                    <img src={require('../'+item.logo)} alt='' />
                                     <span>{item.name}</span>
                                 </div>
                             </div>
@@ -23,7 +80,56 @@ export default class Index extends Component {
                     }) 
                 }
                 </div>
-                <div className='footer'>页脚</div>
+                
+                <div className='footer'>
+                    <div className='documents' >
+                        <p>{documents.title}</p>
+                        <ul>
+                            {
+                                _.map(documents.list, (dd, index) => {
+                                    return(
+                                        <li key={index}>
+                                            <a href={dd.value} target='blank'>{dd.name}</a>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className='workOnline'>
+                        <p>{workOnline.title}</p>
+                        <ul>
+                            {
+                                _.map(workOnline.list, (ww, index) => {
+                                    return(
+                                        <li key={index}>
+                                            <a href={ww.value} target='blank'>{ww.name}</a>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className='friendsUrl'>
+                        <p>{friendsUrl.title}</p>
+                        <ul>
+                            {
+                                _.map(friendsUrl.list, (ff, index) => {
+                                    return(
+                                        <li key={index}>
+                                            <a href={ff.value} target='blank'>{ff.name}</a>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
+                {this.state.showBackTop && 
+                    <div onClick={this.goTop} className='backTop'>
+                        <img src={require('../static/images/btop.png')} alt=''/>
+                    </div>      
+                }      
             </div>
         )
     }
