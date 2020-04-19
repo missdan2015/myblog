8c4541a773331a12c4968e155012533fc24d2dc3
missdan2015 <1070549419@qq.com>
2020-04-19 17:24:27 +0800

    导航栏吸顶效果

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,19 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, Component, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { mainList } from '../../config/page.config';
 import _ from 'lodash';
 import { pageContext } from '../../App';
 import './index.less';
 
-export default function Index(props) {
-    const {state, dispatch} = useContext(pageContext);
-    return (
-        <div className='navigator'>
-            <div className='header'>
-                <h3>Ellen的博客</h3>
-                <p className='tag'>Don't flatter yourself too much</p>
-            </div>
-            <ul>
+
+
+class Tab extends Component{
+  
+    render(){
+        const { state, dispatch, tabFixed} = this.props;
+        return(
+            <div ref='tab_fixed' style={{height: '60px'}}>
+            <ul className={tabFixed ? 'isFixed':''}>
                 
             {
                 _.map(mainList, (item, index) => {
@@ -31,6 +31,38 @@ export default function Index(props) {
                 })
             }
             </ul>
+            </div>
+        )
+    }
+}
+
+function handleScrollThree(tabRef, setTabFixed, tabFixed) {
+    let offsetTop = tabRef.refs.tab_fixed.getBoundingClientRect().top;
+    let isTop = offsetTop < 0 ? true : false;
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (tabFixed !== isTop) {
+        setTabFixed(isTop);
+    }
+    if (scrollTop <200) {
+         setTabFixed(false);
+    }
+}
+
+export default function Index(props) {
+    const {state, dispatch} = useContext(pageContext);
+    const [tabFixed, setTabFixed] = useState(false);
+    const tabRef = useRef(null);
+    useEffect(() => {
+       window.addEventListener('scroll', _.throttle(() => handleScrollThree(tabRef.current, setTabFixed, tabFixed), 100));
+    }, [])
+
+    return (
+        <div className='navigator'>
+            <div className='header'>
+                <h3>Ellen的博客</h3>
+                <p className='tag'>Don't flatter yourself too much</p>
+            </div>
+           <Tab ref={tabRef} state={state} dispatch={dispatch} tabFixed={tabFixed}/>
         </div>
     )
     
